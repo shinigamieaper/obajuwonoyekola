bee0a4a5b990e8d3385fa407787dd29bb5593677
oyekolaobajuwon <oyekolaobajuwon@gmail.com>
2025-03-12 00:00:58 +0100

    Chore: Removed Sentry for now

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,19 +1,19 @@
 "use client";
 
 import * as Sentry from "@sentry/nextjs";
+import Error from "next/error";
 import { useEffect } from "react";
 
 export default function GlobalError({ error }) {
   useEffect(() => {
-    if (error) {
-      Sentry.captureException(error);
-    }
+    Sentry.captureException(error);
   }, [error]);
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <h1 style={{ color: "red" }}>An unexpected error occurred.</h1>
-      <p>{error?.message || "Something went wrong."}</p>
-    </div>
+    <html>
+      <body>
+        <Error />
+      </body>
+    </html>
   );
 }
--- sentry.server.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN || "",
-
-  tracesSampleRate: 1.0, // Adjust in production if needed
-
-  debug: process.env.NODE_ENV === "development", // Enable debug logs in development only
-});
