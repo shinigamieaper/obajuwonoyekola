c7dca0ae6fdfe8a712dd64b7c69b6f9ef37c7a18
Oyekolaobajuwon <150680034+shinigamieaper@users.noreply.github.com>
2025-04-18 19:40:01 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,3 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-import "./globals.css";
-import { ThemeProvider } from "./provider";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Obajuwon's portfolio",
-  description: "Modern Marketing & Development Portfolio",
-};
 
 export default function RootLayout({
   children,
@@ -19,9 +7,27 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      
+      <script
+  dangerouslySetInnerHTML={{
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WMR85QPK');`,
+  }}
+/>
+
       </head>
       <body className={inter.className}>
+      <noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-WMR85QPK"
+    height="0"
+    width="0"
+    style={{ display: 'none', visibility: 'hidden' }}
+  ></iframe>
+</noscript>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
