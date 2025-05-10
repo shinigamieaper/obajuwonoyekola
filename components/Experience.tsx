c54126fcde1ac8e16ce4f806e1568a9121dea23d
oyekolaobajuwon <oyekolaobajuwon@gmail.com>
2025-05-10 16:15:02 +0100

    Replace img with Next/Image in Experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import { workExperience } from "@/data";
 import { Button } from "./ui/MovingBorders";
+import Image from "next/image";
 
 const Experience = () => {
   return (
@@ -35,9 +36,11 @@ const Experience = () => {
               className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
             >
               <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
-                <img
+                <Image
                   src={card.thumbnail}
                   alt={card.thumbnail}
+                  width={128}
+                  height={128}
                   className="lg:w-32 md:w-20 w-16"
                 />
                 <div className="lg:ms-5">
